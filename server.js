2a7d8fedfb4d8a7b883dd19ade1333f9fd835b11
Juan Torres <juanin.torres@gmail.com>
2013-01-31 20:56:22 +0000

    Agregando comentarios

--- server.js
+++ server.js
@@ -4,15 +4,18 @@ var express = require('express'),
 	io      = require('socket.io').listen(server),
 	cons    = require('consolidate');
 
+// Dejamos trabajando el server el el puerto 3000
 server.listen(3000);
 
 app.engine('.html', cons.jade);
 app.set('view engine', 'html');
 
+// Contenido estatico va en la carpeta public
 app.use(express.static('./public'));
 
 app.get('/', function(req, res){
   	res.render('index',{
+                //variable titulo
   		titulo : "Pinta la grilla"
   	});
 });
@@ -24,15 +27,6 @@ io.sockets.on('connection', function (socket) {
 	});
 
 });
-
-
-
-
-
-
-
-
-
 
 
 
